6505361331f050f175c7c4a39a0b6ff6ffa6e263
azee-rajput <azee.rajput.7@gmail.com>
2020-03-23 17:09:59 +0500

    Updates

---
+++ precache-manifest.f1908ea686d8d5bd0c4cc9a0b7b9bb5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fca94b100833b216d9c0fbaed567b0",
+    "url": "/weather-ts/index.html"
+  },
+  {
+    "revision": "c2b5c73fe41ff062f6ef",
+    "url": "/weather-ts/static/css/2.c3384713.chunk.css"
+  },
+  {
+    "revision": "222016e3ef820e64959e",
+    "url": "/weather-ts/static/css/main.6a8a199a.chunk.css"
+  },
+  {
+    "revision": "c2b5c73fe41ff062f6ef",
+    "url": "/weather-ts/static/js/2.db7f58b5.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/weather-ts/static/js/2.db7f58b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222016e3ef820e64959e",
+    "url": "/weather-ts/static/js/main.0be2fa10.chunk.js"
+  },
+  {
+    "revision": "2a6af1be127d50de1273",
+    "url": "/weather-ts/static/js/runtime-main.907a4b98.js"
+  },
+  {
+    "revision": "b80163c58ff6108077df72319a7bef71",
+    "url": "/weather-ts/static/media/myAvatar.b80163c5.png"
+  },
+  {
+    "revision": "b0b3f17e6840538fe0c5309e2b96f329",
+    "url": "/weather-ts/static/media/questrial-latin-400.b0b3f17e.woff"
+  },
+  {
+    "revision": "d7cfa042f25e95e221f73fa59e7d4a9d",
+    "url": "/weather-ts/static/media/questrial-latin-400.d7cfa042.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather4cast/precache-manifest.68652453f21759df17bf8eb51da4df06.js"
+  "/weather-ts/precache-manifest.f1908ea686d8d5bd0c4cc9a0b7b9bb5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather4cast/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-ts/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
